8c23b263f3f107d872da944adf8fd19a317e99de
bingryan <bingryan@outlook.com>
2022-02-12 02:01:16 +0800

    feat(git): add set github auth token logic (#9)

--- src/routes/git.rs
+++ src/routes/git.rs
@@ -2,10 +2,13 @@ use axum::{response::IntoResponse, routing::get, Json, Router};
 use git2::Repository;
 use std::env;
 
+
 use crate::git::repository::Repo;
 
 pub fn router() -> Router {
-    Router::new().route("/api/git/logs", get(git_logs))
+    Router::new()
+        .route("/api/git/logs", get(git_logs))
+        .route("/api/github/authentication", get(github_authentication))
 }
 
 pub(crate) async fn git_logs() -> impl IntoResponse {
@@ -13,4 +16,8 @@ pub(crate) async fn git_logs() -> impl IntoResponse {
     let dis_repo = Repository::discover(&current_dir).unwrap();
     let rep = Repo::new(&dis_repo).unwrap();
     return Json(rep.cache.logs);
+}
+
+pub(crate) async fn github_authentication() -> String {
+    env::var("GITHUB_AUTHENTICATION").unwrap_or("GITHUB_AUTHENTICATION".into())
 }
